b0ca89d04a3ba5b0b859385f11ec07d58ad8b3a7
Pratyush <pratyushsrivastava34@gmail.com>
2025-09-12 10:36:17 +0530

    stream sentry+

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -19,6 +19,12 @@ const syncUser = inngest.createFunction(
         }
 
         await User.create(newUser);
+
+        await upsertStreamUser({
+            id: newUser.clerkId.toString(),
+            name: newUser.name,
+            image: newUser.image,
+        });
     }
 );
 
@@ -29,6 +35,7 @@ const deleteUserFromDB = inngest.createFunction(
         await connectDB();
         const {id} = event.data;
         await User.deleteOne({clerkId: id});
+        
         await deleteStreamUser(id.toString());
     }
 );
---
+++ backend/src/controllers/chat.controller.js
@@ -0,0 +1,12 @@
+import { generateStreamToken } from "../config/stream.js";
+
+export const getStreamToken = async  (req, res) => {
+    try{
+        const token = await generateStreamToken(req.auth().userId);
+
+        res.status(200).json({token});
+    }catch(error){
+        console.log("Error getting Stream token:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}
